fc96101fa9baf64432e5d9505bb2c4da111656c6
henriquefugie <henriquefugie@gmail.com>
2022-10-24 21:37:28 -0300

    automato

--- AutomatoFD.py
+++ AutomatoFD.py
@@ -77,34 +77,18 @@ class AutomatoFD:
         for e in self.estados:
             s += '{}, '.format(str(e))
         s += '} \n'
+        s += ' A = {'
+        for a in self.alfabeto:
+            s += "'{}', ".format(a)
+        s += '} \n'
         s += ' T = {'
         for (e,a) in self.transicoes.keys():
             d = self.transicoes[(e, a)]
             s += "({}, '{}')-->{}, ".format(e, a ,d)
         s += '} \n'
+        s += ' i = {} \n'.format(self.inicial)
         s += ' F = {'
         for e in self.finais:
             s += '{}, '.format(str(e))
         s += '}'
         return s
-
-
-if __name__ == '__main__':
-
-    afd = AutomatoFD('ab')
-
-    for i in range(1, 5):
-        afd.criaEstado(i)
-    afd.mudaEstadoInicial(1)
-    afd.mudaEstadoFinal(4, True)
-
-    afd.criaTransicao(1, 2, 'a')
-    afd.criaTransicao(2, 1, 'a')
-    afd.criaTransicao(3, 4, 'a')
-    afd.criaTransicao(4, 3, 'a')
-    afd.criaTransicao(1, 3, 'b')
-    afd.criaTransicao(2, 1, 'b')
-    afd.criaTransicao(3, 4, 'b')
-    afd.criaTransicao(4, 2, 'b')
-
-    print(afd)
--- main.py
+++ main.py
@@ -1,2 +1,23 @@
-print ('Hello World')
-#TESTANDO 3
+
+from AutomatoFD import AutomatoFD
+
+if __name__ == '__main__':
+
+    afd = AutomatoFD('ab')
+
+    for i in range(1, 5):
+        afd.criaEstado(i)
+    afd.mudaEstadoInicial(1)
+    afd.mudaEstadoFinal(4, True)
+
+    afd.criaTransicao(1, 2, 'a')
+    afd.criaTransicao(2, 1, 'a')
+    afd.criaTransicao(3, 4, 'a')
+    afd.criaTransicao(4, 3, 'a')
+    afd.criaTransicao(1, 3, 'b')
+    afd.criaTransicao(2, 1, 'b')
+    afd.criaTransicao(3, 4, 'b')
+    afd.criaTransicao(4, 2, 'b')
+
+    print(afd)
+    
