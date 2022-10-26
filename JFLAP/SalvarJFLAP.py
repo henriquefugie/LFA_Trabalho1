ee28eacc261bb15eec9e5feed9b21fd15c1d49da
henriquefugie <henriquefugie@gmail.com>
2022-10-25 22:50:25 -0300

    Salvando e lendo JFLAP

--- JFLAP/SalvarJFLAP.py
+++ JFLAP/SalvarJFLAP.py
@@ -1,4 +1,5 @@
-import xml.etree.cElementTree as ETc
+import os
+import xml.etree.ElementTree as ETc
 from AutomatoFD import AutomatoFD
 
 class SalvarJFLAP():
@@ -15,22 +16,17 @@ class SalvarJFLAP():
         doc = ETc.SubElement(root, "automaton")
         
         for e in self.afd.estados:
-            sub =  ETc.SubElement(doc, "state")
-            sub.set('id', e)
-            sub.set('name', e)
+            sub =  ETc.SubElement(doc, "state", id=str(e), name=str(e))
             if e == self.afd.inicial:
                 ETc.SubElement(sub,"initial")
-            if self.afd.estadoFinal:
+            if self.afd.estadoFinal(int(e)):
                 ETc.SubElement(sub, "final")
                 
-        for (e, step) in self.afd.transicoes.keys():
-            for(a, after) in step.items():
-                for d in after:
+        for (e, a) in self.afd.transicoes.keys():
                     tran = ETc.SubElement(doc, "transition")
                     d = self.afd.transicoes[(e, a)]
                     ETc.SubElement(tran, "from").text = str(e)
                     ETc.SubElement(tran, "to").text = str(d)
                     ETc.SubElement(tran, "read").text = str(a)
         tree = ETc.ElementTree(root)
-
-        tree.write("{}.jff".format(self.caminho) ,encoding='utf8', method='xml', short_empty_elements = True)
+        tree.write("{}.jff".format(self.caminho+self.nome) ,encoding="UTF-8", method="xml",xml_declaration=True)
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from JFLAP.LerJFLAP import LerJFLAP
 if __name__ == '__main__':
 
     afd = AutomatoFD('ab')
+    afd1 = AutomatoFD('ab')
 
     for i in range(0, 6):
         afd.criaEstado(i)
@@ -18,15 +19,24 @@ if __name__ == '__main__':
     afd.criaTransicao(2, 1, 'b')
     afd.criaTransicao(3, 4, 'b')
     afd.criaTransicao(4, 2, 'b')
-
     print(afd)
+
+    for i in range(0, 3):
+        afd1.criaEstado(i)
+    afd1.mudaEstadoInicial(0)
+    afd1.mudaEstadoFinal(2, True)
+    afd1.criaTransicao(0, 1, 'a')
+    afd1.criaTransicao(1, 2, 'b')
+    afd1.criaTransicao(2, 2, 'a')
+    afd1.criaTransicao(2, 2, 'b')
+    print(afd1)
     
-    salvar1 = SalvarJFLAP(afd = afd, caminho = "./JFLAP/Exemplos/", nome = "automatoteste")
+    salvar1 = SalvarJFLAP(afd = afd1, caminho = "./JFLAP/Exemplos/", nome = "testando123")
     salvar1.salvarAFD()
     print('automato salvo!')
     
     leitura = LerJFLAP(caminho = "./JFLAP/Exemplos/ComecaComAB.jff")
-    afd = leitura.lerAFD()
-    print(afd)
+    afd1 = leitura.lerAFD()
+    print(afd1)
     print('automato lido')
     
