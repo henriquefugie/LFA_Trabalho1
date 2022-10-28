514662463cb5dd225af2a8d1a1729852f344fb8f
henriquefugie <henriquefugie@gmail.com>
2022-10-28 00:01:48 -0300

    Minimizacao e Equivalencia funcionando

--- main.py
+++ main.py
@@ -11,22 +11,30 @@ def copiaAFD(self):
     
 if __name__ == '__main__':
     
-    leitura2 = LerJFLAP(nome = "prefixo_ab-sufixo_ab")
-    afdA = leitura2.lerAFD()
-    
-    leitura2 = LerJFLAP(nome = "prefixo_ab-sufixo_ab_comVazio")
-    afdB = leitura2.lerAFD()
-    
-    minimiza = MinimizaAFD(afdA)
-    afd = minimiza.minimizaAFD()
-    print(afd)
-    
-    salva = SalvarJFLAP(afd= afd, nome= "AFDminimizado")
-    salva.salvarAFD()
+    leitura = LerJFLAP(nome = "AutomatoParaMinimizar")
+    afdparamin = leitura.lerAFD()
 
-    minimiza = MinimizaAFD(afdB)
-    afdB1 = minimiza.minimizaAFD()
-    print(afdB1)
+    minimiza = MinimizaAFD(afdparamin)
+    afdAmin = minimiza.minimizaAFD()
+    print(afdAmin)
     
-    salva = SalvarJFLAP(afd= afdB1, nome= "AFDminimizadocomVazio")
+    salva = SalvarJFLAP(afd= afdAmin, nome= "AutomatoMinimizado")
     salva.salvarAFD()
+    
+    leitura = LerJFLAP(nome = "AutomatoA")
+    afdA = leitura.lerAFD()
+    print(afdA)
+    
+    leitura = LerJFLAP(nome = "AutomatoB")
+    afdB = leitura.lerAFD()
+    print(afdB)
+    
+    equivalencia = EquivalenciaAFD(afd = afdA)
+    tabelaEstEquivalentes = equivalencia.estadosEquiv()
+    print("Tabela dos estados equivalentes")
+    for (x, y) in tabelaEstEquivalentes.keys():
+        v = tabelaEstEquivalentes[(x, y)]
+        print("({} e {})-->{}, ".format(x, y, v))
+        
+    equivalenciaAFDs = EquivalenciaAFDs(afd1 = afdA, afd2 = afdB)
+    equivalenciaAFDs.EquivalenciaEntreAFDs()
